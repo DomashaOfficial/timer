bf596908c5e14ad5239add0e44cc9ebbb9bad4cb
Aroshakalpana <89211301+Aroshakalpana2468@users.noreply.github.com>
2022-02-12 12:31:33 +0530

    Add files via upload

---
+++ logo.py
@@ -0,0 +1,14 @@
+@app.on_message(filters.command(["logo"]))
+async def logo(_, m : Message):
+    if len(m.command) <2:
+        return await m.reply_text("Please provide a name")
+    else: 
+        try:
+            hee = await m.reply("making your logo...")
+            name = m.text.split(None, 1)[1]
+            req = requests.get(f"https://sd-logo-api.herokuapp.com/?logo={name}")
+            IMG = req.text
+            await hee.delete()
+            await m.reply_photo(IMG) 
+        except Exception as e:
+            await m.reply_text(f"Error: {e}")
